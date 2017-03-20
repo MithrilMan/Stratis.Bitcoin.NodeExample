4a97e3f63dc1022d8d064e6b7aeeece30c8f83cf
MithrilMan <red.angel@libero.it>
2017-03-20 17:57:30 +0100

    first commit

---
+++ src/Stratis.Bitcoin.NodeExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Stratis.Bitcoin.Builder;
+using Stratis.Bitcoin.Configuration;
+using Stratis.Bitcoin.MemoryPool;
+
+namespace Stratis.Bitcoin.NodeExample {
+    public class Program {
+        public static void Main(string[] args) {
+            Logging.Logs.Configure(new LoggerFactory().AddConsole(LogLevel.Trace, false));
+            NodeSettings nodeSettings = NodeSettings.FromArguments(args);
+
+            var node = (FullNode)new FullNodeBuilder()
+                .UseNodeSettings(nodeSettings)
+                .UseMempool()
+                .Build();
+
+            // == shout down thread ==
+            new Thread(() => {
+                Console.WriteLine("Press one key to stop");
+                Console.ReadLine();
+                node.Dispose();
+            }) {
+                IsBackground = true //so the process terminate
+            }.Start();
+
+            // == mining thread ==
+            if (args.Any(a => a.Contains("mine"))) {
+                new Thread(() => {
+                    Thread.Sleep(10000); // let the node start
+                    while (!node.IsDisposed) {
+                        Thread.Sleep(100); // wait 1 sec
+                                           // generate 1 block
+                        var res = node.Miner.GenerateBlocks(new Stratis.Bitcoin.Miner.ReserveScript() { reserveSfullNodecript = new NBitcoin.Key().ScriptPubKey }, 1, int.MaxValue, false);
+                        if (res.Any())
+                            Console.WriteLine("mined tip at: " + node?.Chain.Tip.Height);
+                    }
+                }) {
+                    IsBackground = true //so the process terminate
+                }.Start();
+            }
+
+            node.Start();
+            node.WaitDisposed();
+            node.Dispose();
+        }
+    }
+}
